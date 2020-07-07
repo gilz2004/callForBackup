e8c7e163f1ac99fd559c2ad7ee50bf712f612f3e
Gilz <gilz2004@gmail.com>
2020-07-07 20:53:08 +0300

    ADDING CUSTOM TAB TO FB DOTS MENU

---
+++ src/components/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+import { DiProlog } from 'react-icons/di';
+const AppBox = styled.div`
+  border-top: 1px solid gray;
+  padding: 8px 12px;
+  display: flex;
+  min-height: 24px;
+  align-items: center;
+`;
+
+const ExtensionIcon = styled(DiProlog)`
+  width: 16px;
+  height: 16px;
+`;
+
+const ExtensionText = styled.div`
+  font-size: 14px;
+  font-weight: 600;
+  padding: 0 7px;
+`;
+
+const App = () => {
+  return (
+    <AppBox>
+      <ExtensionIcon />
+      <ExtensionText>Ask friends backup</ExtensionText>
+    </AppBox>
+  );
+};
+
+export default App;
--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,32 @@
-import { printLine } from './modules/print';
+// import { printLine } from './modules/print';
 
-console.log('Content script works!');
-console.log('Must reload extension for modifications to take effect.');
+// console.log('Content script works!');
+// // console.log('Must reload extension for modifications to take effect.');
+// printLine('Welcome to FaceBook extension - By Gil');
+// const menu = document.querySelector('.swg4t2nn');
+// const hr = document.createElement('hr');
+// menu.appendChild(hr);
+// const menuItem = document.querySelector('.oajrlxb2');
 
-printLine("Using the 'printLine' function from the Print Module");
+// const outerMenu = document.querySelector('.o8kakjsu');
+
+import React from 'react';
+import { render } from 'react-dom';
+import App from '../../components/App';
+
+const appendElement = () => {
+  //TODO:ASYNC CALL TO THE MENU MAKES THE FIRST RENDER OF THE MENU TO SHOW NULL.//ON THE SECOND CLICK ALL WORKS.
+  // FIX THIS BUG
+  //AND MAKE SURE EACH ... BUTTON CLICKED THEN OPEN the MENU WITH MY CUSTOM TAB.
+  //AND UNSUBSCRIBE FROM THE EVENT ?
+  const menu = document.querySelector('._54ng');
+  console.log(menu, 'menu');
+  const extension = document.createElement('div');
+  extension.id = 'gilExtension';
+  menu.appendChild(extension);
+  render(<App />, document.getElementById('gilExtension'));
+};
+
+const togglerBtn = document.querySelector('._4xev');
+
+togglerBtn.addEventListener('click', appendElement);
